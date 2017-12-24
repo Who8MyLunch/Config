3cd140156e019c1db670bfd5dd5222a22777d852
Pierre V. Villeneuve <pierre.villeneuve@gmail.com>
2017-12-24 11:04:24 -0800

    updates

--- setup_03.sh
+++ setup_03.sh
@@ -5,17 +5,19 @@
 # Then go to this site (https://www.python.org/downloads) and download the source for Python.
 # Unpack the contents into a folder of your choosing.
 
+ver=3.6.4
+
 cd ~/Projects
-wget https://www.python.org/ftp/python/3.6.3/Python-3.6.3.tar.xz
+wget https://www.python.org/ftp/python/${ver}/Python-${ver}.tar.xz
 
 # Uncompress
-tar xvf Python-3.6.3.tar.xz
+tar xvf Python-${ver}.tar.xz
 
 # Change into that folder and issue the following commands (or similar).
 # Also, use the `prefix` option below to specify building and installing to the user's
 # environment instead of the system's.
 
-cd Python-3.6.3
+cd Python-${ver}
 
 make clean
 ./configure --enable-optimizations --with-lto --prefix=$HOME/.local
