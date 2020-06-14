cc2824fef71379932902a403250e03939a2d6d45
Pierre V. Villeneuve <pierre.villeneuve@gmail.com>
2020-06-14 11:47:15 -0700

    consolidate config files

--- config/copy_files.sh
+++ config/copy_files.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
+#---------------------------------------
+# General stuff in my home folder
+for file in bashrc bash_aliases gitconfig gitignore toprc
+do
+    echo "copying $f"
+    eval cp ./_$file $HOME/.$file
+done
+
+#------------------------------------
+# Python stuff
 # https://pip.readthedocs.io/en/latest/user_guide/#configuration
 file=pip.conf
 dest=$HOME/.config/pip/
+
 mkdir -p $dest
 cp $file $dest
-
 
 # https://towardsdatascience.com/how-to-automatically-import-your-favorite-libraries-into-ipython-or-a-jupyter-notebook-9c69d89aa343
 # http://ipython.readthedocs.io/en/stable/config/extensions/autoreload.html
 file=ipython_startup.ipy
 dest=$HOME/.ipython/profile_default/startup
+
 mkdir -p $dest
 cp $file $dest
 
--- config/copy_rc_files.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for f in bashrc bash_aliases gitconfig gitignore toprc
-do
-    echo "copying $f"
-    eval cp ./_$f $HOME/.$f
-done
