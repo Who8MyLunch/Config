8f2c5975d8d1b3c6a22785a7c5ad727eaf7e4585
Pierre V. Villeneuve <pierre.villeneuve@gmail.com>
2021-01-16 14:01:22 -0800

    edits

--- config/copy_files.sh
+++ config/copy_files.sh
@@ -13,13 +13,6 @@ done
 
 mkdir -p $HOME/wheelhouse   # not sure if this is needed or not???
 
-# https://pip.readthedocs.io/en/latest/user_guide/#configuration
-file=pip.conf
-dest=$HOME/.config/pip/
-
-mkdir -p $dest
-cp $file $dest
-echo "copy $file"
 
 # https://towardsdatascience.com/how-to-automatically-import-your-favorite-libraries-into-ipython-or-a-jupyter-notebook-9c69d89aa343
 # http://ipython.readthedocs.io/en/stable/config/extensions/autoreload.html
